93c095e19485f4196d52c33cda4df243a26863dc
David <41311088+biagiola@users.noreply.github.com>
2020-08-14 09:37:32 -0400

    new intro login

---
+++ login.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { setNewToken, toggleNavbar } from '../actions/actions';
+
+class login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        }
+        this.changeEmail = this.changeEmail.bind(this)
+        this.changePassword = this.changePassword.bind(this)
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    changeEmail (e) {
+        this.setState({
+            email: e.target.value
+        })
+    }
+
+    changePassword (e) {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        
+        const newUser = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        //console.log(newUser)
+        axios.post('http://localhost:5000/user/login', newUser)
+            .then( res => {
+                console.log(':D res: ', res)
+                this.props.setNewToken(res.data.token)
+                this.props.history.push('/products')
+            }).then (
+                this.props.toggleNavbar()
+            )
+            .catch( error => {
+                console.log(error + " :'(");
+            })
+        
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="form-login">
+                    <form onSubmit={ this.onSubmit } className="input-field">
+                        <input 
+                            id="form-email" 
+                            className="form-group"
+                            type="text"
+                            placeholder="Email..."
+                            value={ this.state.email } 
+                            onChange={ this.changeEmail }/>
+                        <br/>
+                        <input 
+                            id="form-password" 
+                            className="form-group"
+                            type="text"
+                            placeholder="Password..."
+                            value={ this.state.password } 
+                            onChange={ this.changePassword }/>
+                        <br/>
+                        <input
+                            id="form-submit-login" 
+                            className="btn"
+                            type="submit"
+                            value="GO"
+                        />
+                    </form>     
+                </div>
+
+                <div className="register-brand">
+                    <div id="register-msg" className="">Sign Up and See What is Next!</div>
+                    <div className="btn btn-register">
+                        <Link to={'/register'} >Register</Link>            
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+login.propTypes = {
+  setNewToken: PropTypes.func,
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setNewToken: token => dispatch(setNewToken(token)),
+    toggleNavbar: token => dispatch(toggleNavbar(token))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(login)
---
+++ product.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+
+class Products extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            product: '',
+            modal: false
+        };
+        this.deleteProduct = this.deleteProduct.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+    
+    componentDidMount() {
+        // get the lenguages before the render
+        axios.get(`http://localhost:5000/products/${this.props.match.params.productId}`)
+            .then( res => {
+                //if( res.data.products > 0 ) {
+                    this.setState({
+                        product: res.data.product
+                    })
+                //}
+            })
+    }
+
+    toggleModal() {
+        this.setState({
+            modal: !this.state.modal
+        })
+    }
+
+    deleteProduct(e) {
+        e.preventDefault()
+
+        const config = {
+            headers: { Authorization: `Bearer ${this.props.authToken}` }
+        };
+        
+        axios.delete( 
+            'http://localhost:5000/products/' + this.props.match.params.productId,
+            config
+        )
+            .then( res => {
+                console.log('product.js delete then ', res)
+                this.props.history.push('/products')
+            })
+            .catch( err => {
+                console.log('product.js delete then ', err)
+                alert('Unauthorized user')
+            })
+    }
+
+    render() {
+
+        const deleteButton = this.props.authToken ? 
+            (<button onClick={ this.toggleModal } className="btn">Delete</button>) 
+            : 
+            ('')
+
+        const imageName = this.state.product ? this.state.product.productImage.slice(8, -4) : ''
+
+        const scope = this.state.modal ? "block" : "none"
+        const position = this.state.modal ? "fixed" : "relative"
+
+        return (
+            <div className="wrapper container">
+
+                <div id="productData" className="productContainer">
+                    <img 
+                        id="big-img"
+                        src={`http://localhost:5000/uploads/${imageName}.jpg` } 
+                        width="370"                        
+                        height="556"
+                        alt={`${this.state.product.name}`}>
+                    </img>
+                    <div className="descriptionContainer">
+                        <h4 className="">{this.state.product.name}</h4>
+                        <h4 className="">{this.state.product.price}$</h4>
+                        <h4>Synapsis</h4>
+                        <p id="synapsis">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+                
+                <div id="productBtns">
+                    <Link to={ '/products'} className="btn">Back</Link>
+                    { deleteButton }
+                </div>
+                
+                <div className="modal modalDelete" style={{ display: scope, position: position  }}>
+                    <div className="modalContent" >
+                        <p>Are you sure to delete it?</p>
+                        <span className="btn" onClick={ this.deleteProduct }>YES</span>
+                        <span className="btn" onClick={ this.toggleModal }>NO</span>
+                    </div>    
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    authToken: state.casa.authToken
+})
+
+export default connect(mapStateToProps, null)(Products);
