42c1e8e3633fd4a56c062d4aa25e373cdb8e4223
David <41311088+biagiola@users.noreply.github.com>
2020-08-19 11:27:30 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,53 +1,35 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import Navbar from './components/navbar';
-import Login from './components/login';
-import Products from './components/products';
-import Product from './components/product';
-import AddProduct from './components/addProduct';
+import './App.css'
+import './App.scss'
+
+import Navbar from './components/navbar'
+import Login from './components/login'
+import Register from './components/register'
+import Products from './components/products'
+import AddProduct from './components/addProduct'
 import Orders from './components/orders';
-import Users from './components/users';
-import Register from './components/register';
-
-import {
-  CSSTransition,
-  TransitionGroup,
-} from 'react-transition-group';
+import Users from './components/users'
 
 function App() {
   return (
     <div className="App wrapper">
       <BrowserRouter>
-        <Navbar/>        
-        <Route render={ ({ location }) => (
-          <TransitionGroup>
-            <CSSTransition
-              in={ true }
-              appear={ true }
-              key={ location.key } 
-              timeout={ 800 }
-              classNames="fade"
-            >
-              <Switch >
-                <Route exact path="/" component={ Login } />
-                <Route path="/add" component={ AddProduct } />
-                <Route path="/products" component={ Products } />
-                <Route path="/:productId" component={ Product } />
-                <Route path="/orders" component={ Orders } />
-                <Route path="/users" component={ Users } />
-                <Route path="/register" component={ Register } />
-                <Route path="/logout" component={ () => <h1>Tschuss</h1> } />
-                <Route path="*" component={ () => "404 not found" } />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        )}/>
+          <Navbar/>        
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/register" component={ Register } />
+            <Route path="/products" component={ Products } />
+            <Route path="/orders" component={ Orders } />
+            <Route path="/add" component={ AddProduct } />
+            <Route path="/users" component={ Users } />
+            <Route path="*" component={ () => "404 not found" } />
+          </Switch>
       </BrowserRouter> 
     </div>
   );
 }
 
-export default App;
+export default App
 
