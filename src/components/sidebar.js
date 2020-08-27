0a8cc1cfc66abc38bcfef06966c7112685a9ed9a
David <41311088+biagiola@users.noreply.github.com>
2020-08-27 11:30:00 -0400

    feature's submenu arrow animation added

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,7 +4,13 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { PropTypes } from 'prop-types'
 
-import { toggleSidebar } from '../actions/actions'
+import { 
+    toggleSidebar, 
+    toggleFeatureSubMenu, 
+    toggleServicesSubMenu,
+    animateDownArrowFeature,
+    animateDownArrowServices
+} from '../actions/actions'
 
 class Sidebar extends Component {
     constructor(props) {
@@ -19,13 +25,43 @@ class Sidebar extends Component {
         this.props.toggleSidebar()
     }
 
+    toggleFeatureSubMenu = () => {
+        this.props.toggleFeatureSubMenu()
+        this.props.animateDownArrowFeature()
+    }
+    
+    toggleServicesSubMenu = () => {
+        this.props.toggleServicesSubMenu()
+        this.props.animateDownArrowServices()
+    }
     render() {
         let scope;
         //console.log('this.props.toggleSidebarValue',this.props.toggleSidebarValue)
         if (this.props.toggleSidebarValue) {
             scope = "sidebar show"
         } else {
-            scope = "sidebar "
+            scope = "sidebar"
+        }
+
+        let showFeature;
+        if (this.props.subMenuFeature) {
+            showFeature = "feat-show show"
+        } else {
+            showFeature = "feat-show"
+        }
+
+        let showServices;
+        if (this.props.subMenuServices) {
+            showServices = "serv-show show1"
+        } else {
+            showServices = "serv-show"
+        }
+
+        let arrowFeature;
+        if (this.props.downArrowFeatureRotate) {
+            arrowFeature = "fas fa-caret-down first rotate"
+        } else {
+            arrowFeature = "fas fa-caret-down first"
         }
 
         return (
@@ -38,22 +74,22 @@ class Sidebar extends Component {
                 <ul>
                     <li className="active"><a href="#">Dashboard</a></li>
                     <li>
-                    <a href="#" className="feat-btn">Features
-                        <span className="fas fa-caret-down first"></span>
-                    </a>
-                    <ul className="feat-show">
-                        <li><a href="#">Pages</a></li>
-                        <li><a href="#">Elements</a></li>
-                    </ul>
+                        <a href="#" className="feat-btn" onClick={ this.toggleFeatureSubMenu }>Features
+                            <span className={ arrowFeature }></span>
+                        </a>
+                        <ul className={ showFeature }>
+                            <li><a href="#">Pages</a></li>
+                            <li><a href="#">Elements</a></li>
+                        </ul>
                     </li>
                     <li>
-                    <a href="#" className="serv-btn">Services
-                        <span className="fas fa-caret-down second"></span>
-                    </a>
-                    <ul className="serv-show">
-                        <li><a href="#">App Design</a></li>
-                        <li><a href="#">Web Design</a></li>
-                    </ul>
+                        <a href="#" className="serv-btn" onClick={ this.toggleServicesSubMenu }>Services
+                            <span className="fas fa-caret-down second"></span>
+                        </a>
+                        <ul className={ showServices }>
+                            <li><a href="#">App Design</a></li>
+                            <li><a href="#">Web Design</a></li>
+                        </ul>
                     </li>
                     <li><a href="#">Portfolio</a></li>
                     <li><a href="#">Overview</a></li>
@@ -68,15 +104,25 @@ class Sidebar extends Component {
 Sidebar.propTypes = {
   toggleSidebarValue: PropTypes.bool,
   toggleSidebar: PropTypes.func,
+  animateDownArrowFeature: PropTypes.func,
+  animateDownArrowServices: PropTypes.func,
 }
 
 const mapStateToPros = state => ({
-  toggleSidebarValue: state.all.toggleSidebarValue
+  toggleSidebarValue: state.all.toggleSidebarValue,
+  subMenuFeature: state.all.subMenuFeature,
+  subMenuServices: state.all.subMenuServices,
+  downArrowFeatureRotate: state.all.downArrowFeatureRotate,
+  downArrowServicesRotate:state.all.downArrowServicesRotate
 })
 
 const mapDispatchToProps = dispatch => {
   return {
-    toggleSidebar: () => dispatch(toggleSidebar())
+    toggleSidebar: () => dispatch(toggleSidebar()),
+    toggleFeatureSubMenu: () => dispatch(toggleFeatureSubMenu()),
+    toggleServicesSubMenu: () => dispatch(toggleServicesSubMenu()),
+    animateDownArrowFeature: () => dispatch(animateDownArrowFeature()),
+    animateDownArrowServices: () => dispatch(animateDownArrowServices()),
   }
 }
 
